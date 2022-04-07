ebe0bffd0f0d1e59d8b3894f764381c1bc633d88
BioXsec <96635023+BioXsec@users.noreply.github.com>
2022-04-07 02:54:09 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+import webbrowser, time
+
+
+url = input("Enter url: ")  
+views = input("Enter amount of views: ")
+duration = input("Enter duration before opening another video (in seconds): ")
+
+for i in range(int(views)):
+    webbrowser.open_new(url)
+    time.sleep(int(duration))
+    
+    
+    #Open this in Pycharm or Visual Studio. Not working in Replit!
